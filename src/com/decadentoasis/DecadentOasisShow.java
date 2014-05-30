3e0d910c9867ead64e6210256c22105934fdec50
robot-head <evil@heroicrobot.com>
2014-05-30 09:00:52 -0700

    Making skeleton of processing sketch

---
+++ src/com/decadentoasis/DecadentOasisShow.java
@@ -0,0 +1,31 @@
+package com.decadentoasis;
+
+import heronarts.lx.LX;
+import heronarts.lx.parameter.BasicParameter;
+import heronarts.lx.parameter.LXParameter;
+import heronarts.lx.pattern.LXPattern;
+import heronarts.lx.transition.DissolveTransition;
+import heronarts.lx.transition.LXTransition;
+
+import com.decadentoasis.patterns.*;
+
+public class DecadentOasisShow {
+
+	private LX lx;
+
+	public DecadentOasisShow(LX lx) {
+		this.lx = lx;
+	}
+
+	final BasicParameter dissolveTime = new BasicParameter("DSLV", 400, 50,
+			1000);
+
+	LXPattern[] patterns(LX lx) {
+		LXPattern[] patterns = new LXPattern[] { new SolidColor(lx), };
+		LXTransition t = new DissolveTransition(lx).setDuration(dissolveTime);
+		for (LXPattern p : patterns) {
+			p.setTransition(t);
+		}
+		return patterns;
+	}
+}
