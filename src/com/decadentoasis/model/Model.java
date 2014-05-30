9d2fcedb5f9e566c516694daddedefce7061508b
robot-head <evil@heroicrobot.com>
2014-05-30 14:05:36 -0700

    Submit base model class

--- src/com/decadentoasis/model/Model.java
+++ src/com/decadentoasis/model/Model.java
@@ -1,7 +1,23 @@
 package com.decadentoasis.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import heronarts.lx.model.LXAbstractFixture;
 import heronarts.lx.model.LXModel;
 
 public class Model extends LXModel {
+
+	Model() {
+		super(new Fixture());
+		Fixture f = (Fixture) this.fixtures.get(0);
+	}
+
+	private static class Fixture extends LXAbstractFixture {
+
+		private Fixture() {
+
+		}
+	}
 
 }
