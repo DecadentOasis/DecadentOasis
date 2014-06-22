9b7dd001ee7d7015bd2e30be9430fc751b7d5307
robot-head <evil@heroicrobot.com>
2014-06-22 16:19:46 -0700

    Implement tree types and LED bar creation

--- src/org/dcdnt/model/Tree.java
+++ src/org/dcdnt/model/Tree.java
@@ -1,5 +1,6 @@
 package org.dcdnt.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -30,10 +31,13 @@ public class Tree extends LXModel {
 	 */
 	public final float z;
 
+	/**
+	 * List of LEDBars on the tree
+	 */
 	public final List<LEDBar> ledBars;
 
-	Tree(TreeType type, float x, float y, float z) {
-		super(new Fixture(type, x, y, z));
+	Tree(TreeType type, float x, float y, float z, String pusherMac, int stripNo) {
+		super(new Fixture(type, x, y, z, pusherMac, stripNo));
 		Fixture f = (Fixture) this.fixtures.get(0);
 		this.x = x;
 		this.y = y;
@@ -44,19 +48,62 @@ public class Tree extends LXModel {
 	private static class Fixture extends LXAbstractFixture {
 		final List<LEDBar> ledBars;
 
-		Fixture(TreeType type, float x, float y, float z) {
+		Fixture(TreeType type, float x, float y, float z, String pusherMac,
+				int stripNo) {
+			LXTransform transform = new LXTransform();
+			transform.translate(x, y, z);
+			this.ledBars = new ArrayList<LEDBar>();
+			int pixelNo = 0;
+			float spaceBetweenLeds;
+			int armLength;
 			switch (type) {
 			case SIX_ARM:
-				this.ledBars = Arrays.asList(new LEDBar[] {});
+				armLength = 100;
+				spaceBetweenLeds = armLength / 4;
+				for (int armNumber = 0; armNumber < 6; armNumber++) {
+					this.ledBars.add(new LEDBar(transform,
+							spaceBetweenLeds * 1, 360 / armNumber, pusherMac,
+							stripNo, pixelNo++));
+					this.ledBars.add(new LEDBar(transform,
+							spaceBetweenLeds * 3, 360 / armNumber, pusherMac,
+							stripNo, pixelNo++));
+					this.ledBars.add(new LEDBar(transform,
+							spaceBetweenLeds * 2, 360 / armNumber, pusherMac,
+							stripNo, pixelNo++));
+					this.ledBars.add(new LEDBar(transform,
+							spaceBetweenLeds * 0, 360 / armNumber, pusherMac,
+							stripNo, pixelNo++));
+				}
+
 				break;
 			case EIGHT_ARM:
-				this.ledBars = Arrays.asList(new LEDBar[] {});
+				armLength = 100;
+				spaceBetweenLeds = armLength / 3;
+				for (int armNumber = 0; armNumber < 8; armNumber++) {
+					this.ledBars.add(new LEDBar(transform,
+							spaceBetweenLeds * 0, 360 / armNumber, pusherMac,
+							stripNo, pixelNo++));
+					this.ledBars.add(new LEDBar(transform,
+							spaceBetweenLeds * 2, 360 / armNumber, pusherMac,
+							stripNo, pixelNo++));
+					this.ledBars.add(new LEDBar(transform,
+							spaceBetweenLeds * 1, 360 / armNumber, pusherMac,
+							stripNo, pixelNo++));
+				}
 				break;
 			case TWELVE_ARM:
-				this.ledBars = Arrays.asList(new LEDBar[] {});
+				armLength = 100;
+				spaceBetweenLeds = armLength / 2;
+				for (int armNumber = 0; armNumber < 12; armNumber++) {
+					this.ledBars.add(new LEDBar(transform,
+							spaceBetweenLeds * 0, 360 / armNumber, pusherMac,
+							stripNo, pixelNo++));
+					this.ledBars.add(new LEDBar(transform,
+							spaceBetweenLeds * 1, 360 / armNumber, pusherMac,
+							stripNo, pixelNo++));
+				}
 				break;
 			default:
-				this.ledBars = Arrays.asList(new LEDBar[] {});
 				break;
 			}
 
